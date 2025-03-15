dee221ce93cb2e2afacc2b0e95209cee9c5780c7
MegurineX <root@ip-172-31-31-14.ap-southeast-1.compute.internal>
2025-03-15 13:32:01 +0000

    Initial commit: derpfest-15 device tree for Xiaomi Redmi 13C/POCO C65 (gale/gust)

---
+++ derp_gale.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/xiaomi/gale/device.mk)
+
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+DERP_BUILDTYPE := UNOFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+
+# Gapps
+WITH_GMS := true
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+USE_LEGACY_BOOTANIMATION := true
+
+# Charging Animation
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+PRODUCT_NAME := derp_gale
+PRODUCT_DEVICE := gale
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := 23106RN0DA
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="vnd_gale-user 12 SP1A.210812.016 V816.0.5.0.UGPMIXM release-keys"
+    BuildFingerprint=alps/vnd_gale/gale:12/SP1A.210812.016/V816.0.5.0.UGPMIXM:user/release-keys
